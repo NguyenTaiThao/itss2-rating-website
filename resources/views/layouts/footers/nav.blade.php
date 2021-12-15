05709c110e560539195da642133b4ec28e96f3cb
minhhangtran <hangtm1@solashi.com>
2021-12-15 17:31:40 +0700

    fix: fix footer

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,12 +1,13 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
-            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">アップディビジョン</a>
+            <!-- &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1"
+                target="_blank">Creative Tim</a> &amp;
+            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">アップディビジョン</a> -->
         </div>
     </div>
     <div class="col-xl-6">
-        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+        <!-- <ul class="nav nav-footer justify-content-center justify-content-xl-end">
             <li class="nav-item">
                 <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
             </li>
@@ -19,6 +20,6 @@
             <li class="nav-item">
                 <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">ブログ</a>
             </li>
-        </ul>
+        </ul> -->
     </div>
 </div>
