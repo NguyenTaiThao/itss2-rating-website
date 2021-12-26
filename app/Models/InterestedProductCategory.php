4776588f75ac8de781ae2ba2866bbc540b3a9050
minhhangtran <hangtm1@solashi.com>
2021-12-26 13:38:42 +0700

    feat:  chon brand va product co hung thu

---
+++ app/Models/InterestedProductCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InterestedProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'interested_product_categories';
+
+    protected $fillable = ['user_id', 'product_category_id'];
+}
