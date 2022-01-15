7b6e547a5b4c72b324436640f911469d2463d52a
minhhangtran <hangtm1@solashi.com>
2022-01-15 21:38:14 +0700

    xoa code

--- app/Http/Controllers/Brand/Auth/RegisterController.php
+++ app/Http/Controllers/Brand/Auth/RegisterController.php
@@ -55,8 +55,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:brands,email'],
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'unique:brands,email'],
             'password' => ['required', 'string', 'confirmed'],
             'company_category_id' => ['required', 'exists:company_categories,id'],
             'logo' => ['required', 'image']
